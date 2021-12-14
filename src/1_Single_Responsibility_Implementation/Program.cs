053831ce1c76d387287bd5c4d38d713d46c3ae35
hendra <hendra.wong@inflection.com>
2021-12-13 21:00:29 -0800

    Break the original problem with the implementation.

---
+++ src/1_Single_Responsibility_Implementation/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+Alert alert = new Alert();
+alert.Add("Alert #1");
+alert.Add("Alert #2");
+Console.WriteLine($"Alerts: {alert.ToString()}");
+
+DbPersistence dbPersistence = new DbPersistence();
+dbPersistence.Save(alert);
+
+Logging logging = new Logging();
+logging.WriteLog(alert);
+
+// ********************************************************************
+public class Alert 
+{
+	private List<string> _alerts = new List<string>();
+
+	public int Count { get { return _alerts.Count(); } }
+
+	public void Add(string name) {
+		_alerts.Add(name);
+	}
+
+	public override string ToString() {
+		return string.Join(", ", _alerts);
+	}
+}
+
+public class DbPersistence
+{
+	public void Save(Alert alert)
+	{
+		Console.WriteLine($"Saving { alert.Count } alert(s) to database");
+	}
+}
+
+public class Logging
+{
+	public void WriteLog(Alert alert)
+	{
+		Console.WriteLine($"Log: { alert.ToString() }.");
+	}
+}
